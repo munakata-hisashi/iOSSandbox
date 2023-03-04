3d56a019f622b2ddb5e9c0c3c86823402aa67427
munakata.hisashi <h.grasshopper2@gmail.com>
2023-03-04 19:35:26 +0900

    initial commit

---
+++ iOSSandbox/iOSSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSSandboxApp.swift
+//  iOSSandbox
+//
+//  Created by 宗像恒 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct iOSSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
